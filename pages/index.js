78523300bdb62a4c311005756acbfc1184caf359
ahribori <ahribori@gmail.com>
2018-06-28 00:04:11 +0900

    Add Redux

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,30 @@
-export default () => <div>Welcome to next.js!</div>
+import React from 'react';
+import { connect } from 'react-redux';
+import { startClock, serverRenderClock } from '../store';
+import Examples from '../components/examples';
+
+class Index extends React.Component {
+    static getInitialProps({ reduxStore, req }) {
+        const isServer = !!req;
+        reduxStore.dispatch(serverRenderClock(isServer));
+
+        return {};
+    }
+
+    componentDidMount() {
+        const { dispatch } = this.props;
+        this.timer = startClock(dispatch);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timer);
+    }
+
+    render() {
+        return (
+            <Examples/>
+        );
+    }
+}
+
+export default connect()(Index);
