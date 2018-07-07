ac20150c0352272375be9022e5a6dc7d102d2776
ahribori <ahribori@gmail.com>
2018-07-08 02:09:10 +0900

    Decorator 다시 적용

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,6 +5,8 @@ import { startClock, serverRenderClock, fetch } from '../store/Example';
 import Examples from '../components/examples';
 import { Link } from '../routes';
 
+@withRouter
+@connect()
 class Index extends React.Component {
     static async getInitialProps({ reduxStore, req }) {
         const isServer = !!req;
@@ -38,4 +40,4 @@ class Index extends React.Component {
     }
 }
 
-export default withRouter(connect()(Index));
+export default Index;
